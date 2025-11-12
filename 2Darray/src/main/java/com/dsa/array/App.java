1cc408d697a30b2c1705b9ed2e80780ce554db1f
Nikita Bansode <bansodenikita66@gmail.com>
2025-11-12 19:42:21 +0530

    Greenhouse data in 2D

---
+++ 2Darray/src/main/java/com/dsa/array/App.java
@@ -0,0 +1,16 @@
+package com.dsa.array;
+import com.dsa.array.Greenhouse;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+        Greenhouse g1=new Greenhouse();
+        g1.initializeData();
+        g1.printdata();
+    }
+}
