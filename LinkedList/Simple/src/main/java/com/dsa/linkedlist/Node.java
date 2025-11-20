e8d05e82922813b5cf9a3d5340c5a3d3ff12e37c
Nikita Bansode <bansodenikita66@gmail.com>
2025-11-20 11:42:48 +0530

    Linked List implementation

---
+++ LinkedList/Simple/src/main/java/com/dsa/linkedlist/Node.java
@@ -0,0 +1,16 @@
+package com.dsa.linkedlist;
+
+public class Node{
+    Node next;
+    int data;
+
+    public Node(){
+        next=null;
+        data=0;
+    }
+
+    public Node(int data){
+        next=null;
+        this.data=data;
+    }
+}
