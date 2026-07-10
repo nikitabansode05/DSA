ac09f71d525c680e0678a19e66ddae49627637f6
Nikita Bansode <bansodenikita66@gmail.com>
2026-07-10 16:01:52 +0530

    Queue CRUD

---
+++ Queue/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        QueueInt queue = new QueueInt(4);
+        queue.insert(2);
+        queue.display();
+
+        queue.insert(3);
+        queue.insert(4);
+        queue.insert(5);
+
+        queue.display();
+
+        queue.update(5,7);
+        queue.display();
+
+        queue.delete();
+        queue.display();
+
+        queue.delete();
+        queue.display();
+    }
+}
---
+++ Queue/QueueInt.cs
@@ -0,0 +1,56 @@
+public class QueueInt
+{
+    public int[] queue;
+
+    public QueueInt()
+    {
+        queue=null;
+    }
+
+    public QueueInt(int data)
+    {
+        queue=new int[data];
+    }
+
+    public void insert(int data)
+    {
+      for(int i = 0; i < queue.Length; i++)
+        {
+            if (queue[i] == 0)
+            {
+                queue[i]=data;
+                break;
+            }
+        }
+    }
+
+    public void update(int oldValue,int newValue)
+    {
+        for(int i = 0; i < queue.Length; i++)
+        {
+            if (queue[i] == oldValue)
+            {
+                queue[i]=newValue;
+                break;
+            }
+        }
+    }
+
+    public void delete()
+    {
+        for(int i = 0; i < queue.Length-1; i++)
+        {
+           queue[i]=queue[i+1];
+        }
+        queue[queue.Length-1]=0;
+    }
+
+    public void display()
+    {
+        foreach(int q in queue)
+        {
+            Console.Write(q+" ");
+        }
+        Console.WriteLine("\n*****************************************");
+    }
+}
